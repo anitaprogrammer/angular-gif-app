8a2236d4eece7baad824e5a7b2c25c4706abc1df
Ana Hernandez <ana.hernandez@claroshop.com>
2024-02-21 19:42:01 -0600

    fin seccion 8

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html'
+})
+
+export default class LazyImageComponent implements OnInit{
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+   if(!this.url){
+     throw new Error('Url property is required');
+   }
+  }
+
+  onLoad(){
+    setTimeout(() =>{
+      this.hasLoaded = true;
+    },1000);
+  }
+
+}
